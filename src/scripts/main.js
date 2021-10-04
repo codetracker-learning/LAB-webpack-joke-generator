a58c546e57062671c8dbca12c4040dcbc89f8d4f
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-10-04 06:27:56 -0500

    Added instructor comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+// INSTRUCTORS: DO NOT directly edit this sandbox. Fork it, and place the fork inside the cohort folder.
+
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 import jokeData from './helpers/jokeData';
