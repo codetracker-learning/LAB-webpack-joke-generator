9a96011cc704d1b28774d8b9aa6876c30c4a2825
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-05-10 14:20:34 -0500

    Update jokeData.js

--- src/scripts/helpers/jokeData.js
+++ src/scripts/helpers/jokeData.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 const endpoint = 'https://v2.jokeapi.dev/joke/Programming?safe-mode&type=twopart';
 
-const getJoke = async () => {
-  const jokeCall = await axios.get(endpoint);
-
-  return jokeCall.data;
-};
+const getJoke = () => new Promise((resolve, reject) => {
+  axios.get(endpoint).then((response) => {
+    resolve(response.data);
+  }).catch(error => reject(error));
+});
 
 export default getJoke;
