43e7df690f8d50bb87b81a28209b1c592755b499
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-05-10 15:58:49 -0500

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,11 @@
-// INSTRUCTORS: DO NOT directly edit this sandbox. Fork it, and place the fork inside the cohort folder.
-
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
-import jokeData from '../api/jokeData';
+import htmlStructure from '../components/htmlStructure';
+import events from './events';
 
 const init = () => {
-  document.querySelector('#app').innerHTML = `
-  `;
-
-  jokeData().then(console.warn);
+  htmlStructure();
+  events();
 };
 
 init();
